e3d36fc8180080495296e609569380dfbe263ad5
whitebrain0242 <3047332586@qq.com>
2026-05-15 22:06:43 +0800

    完成所有功能

--- myls.c
+++ myls.c
@@ -1,19 +1,30 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <dirent.h>
 #include <errno.h>
-#include <string.h>
-#include <stdlib.h>
+
 #include <sys/stat.h>
+#include <sys/types.h>
+
+#include <unistd.h>
+#include <limits.h>
+
 #include <pwd.h>
 #include <grp.h>
+
 #include <time.h>
+#include <linux/limits.h>
 // 储存所有选项名称
 typedef struct
 {
     int show_all;    //-a
     int long_format; //-l
+
     int show_inode;  //-i
     int show_blocks; //-s 磁盘数
+
+    int recursive;//-R递归
     int sort_time;   // t,按照时间修改顺序 新-旧
     int reverse;     //-r, 反转排序
 } Options;
@@ -26,7 +37,7 @@ typedef struct
     struct stat st; // 文件属性
 } FileInfo;
 
-// 解析命令行参数
+//看看需要实现哪些功能？？参数解析
 void parse_options(int argc, char *argv[], Options *opts)
 {
     int i, j;
@@ -62,6 +73,9 @@ void parse_options(int argc, char *argv[], Options *opts)
             case 'r':
                 opts->reverse = 1;
                 break;
+            case 'R':
+            opts ->recursive=1;
+            break;
             default:
                 fprintf(stderr,
                         "myls:invalid option -- '%c'\n",
@@ -85,6 +99,51 @@ char *join_path(const char *dir, const char *name)
     }
     sprintf(path, "%s/%s", dir, name);
     return path;
+}
+
+//mode，创建mode权限数组
+void mode_to_string(mode_t mode,char*str){
+    if(S_ISREG(mode))//是否是普通文件？
+    str[0]='-';
+    else if(S_ISDIR(mode))//目录u
+    str[0]='d';
+    else if(S_ISLNK(mode))//链接
+str[0] = 'l';
+    else if(S_ISCHR(mode))//字符设备：键盘，显示器，鼠标
+    str[0] = 'c';
+    else if(S_ISBLK(mode))//块设备（硬盘，u盘
+        str[0] = 'b';
+    else if(S_ISFIFO(mode))//管道文件
+        str[0] = 'p';
+    else if(S_ISSOCK(mode))//是不是套接字文件
+        str[0] = 's';
+    else
+        str[0] = '?';
+
+    //文件所有者
+    str[1] = (mode & S_IRUSR) ? 'r' : '-';//读
+    str[2] = (mode & S_IWUSR) ? 'w' : '-';//写
+    str[3] = (mode & S_IXUSR) ? 'x' : '-';//执行
+    //组成员
+
+    str[4] = (mode & S_IRGRP) ? 'r' : '-';
+    str[5] = (mode & S_IWGRP) ? 'w' : '-';
+    str[6] = (mode & S_IXGRP) ? 'x' : '-';
+    //其他用户
+
+    str[7] = (mode & S_IROTH) ? 'r' : '-';
+    str[8] = (mode & S_IWOTH) ? 'w' : '-';
+    str[9] = (mode & S_IXOTH) ? 'x' : '-';
+
+    str[10] = '\0';
+}
+
+//时间格式化:也就是把时间戳变成年月日
+//传入时间戳
+void format_time(time_t t,char *buf,int size){
+    struct tm* tm_info;//存储年月日是分秒
+    tm_info=localtime(&t);//把时间戳转变年月日是分
+    strftime(buf,size,"%Y-%m-%d %H:%M",tm_info);//格式化称字符串
 }
 
 // 读取目录内文件信息数组，名字路径属性
@@ -128,12 +187,15 @@ FileInfo *read_dir_entries(const char *path, Options *opts, int *count)
         if (*count >= capacity)
         {
             capacity *= 2;
-            files = realloc(files, sizeof(FileInfo) * capacity);
-            if (files == NULL)
+            FileInfo*tmp;
+            tmp = realloc(files, sizeof(FileInfo) * capacity);
+            if (tmp == NULL)
             {
+                free(files);
                 closedir(dir);
                 return NULL;
             }
+            files=tmp;
         }
 
         // 想要获得信息，1.先得到完整路径2.获得文件属性3.保存到信息数组里面
@@ -203,7 +265,76 @@ qsort(files,count,sizeof(FileInfo),compare_time);
     }
 }
 
-// 输出文件信息
+//长格式输出
+void print_long(FileInfo *file,Options*opts){
+    char mode_str[11];
+    struct passwd*pw;//储存用户信息，用户名UID等
+    struct group *gr;
+    char time_buf[64];//存放格式化后的时间
+
+    //权限便成为字符串
+    mode_to_string(file->st.st_mode,mode_str);
+    //时间戳格式化
+    format_time(file->st.st_mtime,time_buf,sizeof(time_buf));
+    //把iD数字变成用户名
+    pw=getpwuid(file->st.st_uid);
+    //组ID变成组名
+    gr=getgrgid(file->st.st_gid);
+
+
+    //顺序打印inode,blocks,mode,应连接数量,用户名小组名，文件大小，时间
+    if (opts->show_inode)
+        {
+            printf("%-8lu ", file->st.st_ino);
+        }
+        // 打印的是文件大小，磁盘数量和文件大小不一样
+        if (opts->show_blocks)
+        {
+            printf("%-4ld ",
+                   (file->st.st_blocks + 1) / 2);
+        }
+        printf("%s ",
+           mode_str);
+
+    printf("%-3lu ",
+           file->st.st_nlink);
+
+    if(pw)
+        printf("%-8s ",
+               pw->pw_name);
+    else
+        printf("%-8d ",
+               file->st.st_uid);
+
+    if(gr)
+        printf("%-8s ",
+               gr->gr_name);
+    else
+        printf("%-8d ",
+               file->st.st_gid);
+
+    printf("%8ld ",
+           file->st.st_size);
+
+    printf("%s ",
+           time_buf);
+
+
+           //软连接显示：如果文件是软链接，就在文件名后面打印：-> 指向的文件
+    if(S_ISLNK(file->st.st_mode)){
+        char target[PATH_MAX];
+        ssize_t len;
+        len=readlink(file->path,target,sizeof(target)-1);
+        if(len!=-1){
+            target[len]='\0';
+            printf("->%s",target);
+        }
+    }
+    printf("\n");
+
+}
+
+// 普通输出
 void print_entries(FileInfo *files, int count, Options *opts)
 {
     // 这个函数实现打印inode和块大小,文件名字
@@ -225,15 +356,26 @@ step=1;
     }
     for (i = start; i !=end; i+=step)
     {
-        if (opts->show_inode)
+        if(opts->long_format)
         {
-            printf("%lu ", files[i].st.st_ino);
+            print_long(&files[i],
+                       opts);
         }
-        // 打印的是文件大小，磁盘数量和文件大小不一样
-        if (opts->show_blocks)
+        else
         {
-            printf("%ld ",
-                   (files[i].st.st_blocks + 1) / 2);
+            if(opts->show_inode)
+            {
+                printf("%-8lu ",
+                       files[i].st.st_ino);
+            }
+
+            if(opts->show_blocks)
+            {
+                printf("%-4ld ",
+                       (files[i].st.st_blocks + 1) / 2);
+            }
+
+            printf("\n");
         }
 
         printf("%s\n", files[i].name);
@@ -253,7 +395,7 @@ void free_entries(FileInfo *files, int count)
     free(files);
 }
 
-// 列出目录
+// 列出目录，实现递归
 //  参数path目录路径,show_title是否显示目录标题（输入多个目录的是时候
 int list_dir(const char *path, int show_title, Options *opts)
 {
@@ -274,6 +416,25 @@ int list_dir(const char *path, int show_title, Options *opts)
 
     // 输出
     print_entries(files, count, opts);
+
+    //-R实现
+    if(opts->recursive){
+        for(int i=0;i<count;i++){
+            if(S_ISDIR(files[i].st.st_mode)){
+                //跳过.和..，不然就进入死循环了
+                if(strcmp(files[i].name,
+                          ".") == 0 ||
+                   strcmp(files[i].name,
+                          "..") == 0)
+                {
+                    continue;
+                }
+                printf("\n");
+                list_dir(files[i].path,1,opts);
+            }
+        }
+    }
+    
 
     // 释放
     free_entries(files, count);
